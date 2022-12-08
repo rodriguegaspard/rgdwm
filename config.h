01fce6fc883b0077001876411fcd1fd60ff34a19
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2022-12-08 00:03:17 +0000

    Tweaked font sizes and rules

--- config.h
+++ config.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Iosevka:size=12", "fontawesome:size=12" };
-static const char dmenufont[]       = "Iosevka:size=12";
+static const char *fonts[]          = { "Iosevka:pixelsize=14:antialias=true:autohint=true", "fontawesome:size=12", "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true"};
+static const char dmenufont[]       = "Iosevka:pixelsize=14:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
