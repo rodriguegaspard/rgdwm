5173fa99fda0dd945e1d0e32fca82f8a334716e8
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2022-12-08 14:34:49 +0000

    Tweaked colors, to try and make a pleasing blue theme

--- config.h
+++ config.h
@@ -1,18 +1,18 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka:pixelsize=14:antialias=true:autohint=true", "fontawesome:size=12", "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true"};
 static const char dmenufont[]       = "Iosevka:pixelsize=14:antialias=true:autohint=true";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#003366";
+static const char col_gray2[]       = "#708090";
+static const char col_gray3[]       = "#f0f8ff";
+static const char col_gray4[]       = "#f0ffff";
+static const char col_cyan[]        = "#006994"; 
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
