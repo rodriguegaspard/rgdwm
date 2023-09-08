d85fe19fd21c5260ab00cf304d7d0deba37e3eae
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2023-09-08 18:10:29 +0800

    Applied alpha patch for opacity, and tweaked color scheme a bit

--- config.h
+++ config.h
@@ -1,22 +1,38 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 10;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Iosevka:pixelsize=14:antialias=true:autohint=true", "fontawesome:size=12", "Noto Color Emoji:pixelsize=14:antialias=true:autohint=true"};
 static const char dmenufont[]       = "Iosevka:pixelsize=14:antialias=true:autohint=true";
-static const char col_gray1[]       = "#003366";
-static const char col_gray2[]       = "#708090";
-static const char col_gray3[]       = "#f0f8ff";
-static const char col_gray4[]       = "#f0ffff";
-static const char col_cyan[]        = "#006994"; 
+//static const char col_gray1[]       = "#003366";
+//static const char col_gray2[]       = "#708090";
+//static const char col_gray3[]       = "#f0f8ff";
+//static const char col_gray4[]       = "#f0ffff";
+//static const char col_cyan[]        = "#006994";
+
+// Blue theme colors
+static const char col_fg_norm[]		= "#b0e0e6"; // Powder Blue
+static const char col_fg_sel[]		= "#f0f8ff"; // Alice Blue
+static const char col_bg_norm[]		= "#23297a"; // St. Patrick Blue
+static const char col_bg_sel[]		= "#318ce7"; // Bleu De France
+static const char col_border_norm[]	= "#73a9c2"; // Moonstone Blue
+static const char col_border_sel[]	= "#0f52ba"; // Sapphire
+
+static const unsigned int baralpha = 150; // 0xd0 for opaque
+static const unsigned int borderalpha = 150; // OPAQUE for opaque
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg_norm, col_bg_norm, col_border_norm },
+	[SchemeSel]  = { col_fg_sel, col_bg_sel,  col_border_sel },
+};
+static const unsigned int alphas[][3]      = {
+    /*               fg      bg        border*/
+    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, 200, 200 },
 };
 
 /* tagging */
@@ -58,7 +74,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg_norm, "-nf", col_fg_norm, "-sb", col_bg_sel, "-sf", col_fg_sel, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *volume_mute[]  = { "togglevolume", NULL };
 static const char *volume_down[]  = { "volumedown", NULL };
