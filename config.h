2b65867389cafc16334fef699939815744041d62
Rodrigue Gaspard <rodriguegaspard@gmail.com>
2026-01-18 14:03:35 +0100

    Applied functionalgaps patch

--- config.h
+++ config.h
@@ -1,27 +1,38 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const int startwithgaps	    = 1;	 /* 1 means gaps are used by default */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static unsigned int baralpha        = 0xd0;
-static unsigned int borderalpha     = OPAQUE;
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Iosevka Nerd Font:pixelsize=16:antialias=true:autohint=true", "Noto Color Emoji:pixelsize=16:antialias=true:autohint=true"};
+static const char dmenufont[]       = "Iosevka Nerd Font:pixelsize=16:antialias=true:autohint=true";
+
+// Blue theme colors
+static const char col_fg_norm[]		    = "#b0e0e6"; // Powder Blue
+static const char col_fg_sel[]		    = "#e7feff"; // Bubbles
+static const char col_bg_norm[]		    = "#23297a"; // St. Patrick Blue
+static const char col_bg_sel[]		    = "#318ce7"; // Bleu De France
+static const char col_border_norm[]	  = "#73a9c2"; // Moonstone Blue
+static const char col_border_sel[]	  = "#0f52ba"; // Sapphire
+
+static const unsigned int baralpha = 150; // 0xd0 for opaque
+static const unsigned int borderalpha = 150; // OPAQUE for opaque
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_fg_norm, col_bg_norm, col_border_norm },
+	[SchemeSel]  = { col_fg_sel, col_bg_sel,  col_border_sel },
+};
+static const unsigned int alphas[][3]      = {
+    /*               fg      bg        border*/
+    [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, 200, 200 },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "󰎕", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -42,13 +53,13 @@ static const int refreshrate = 120;  /* refresh rate (per second) for client mov
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "",      tile },    /* first entry is default */
+	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "",      monocle },
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -60,13 +71,24 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_bg_norm, "-nf", col_fg_norm, "-sb", col_bg_sel, "-sf", col_fg_sel, NULL };
+static const char *termcmd[]  = { "kitty", NULL };
+static const char *voltoggle[]  = { "volumecontrol", "-t", NULL };
+static const char *voldown[]  = { "volumecontrol", "-d", NULL };
+static const char *volup[]  = { "volumecontrol", "-i", NULL };
+static const char *brightness_down[]  = { "xbacklight", "-dec", "5", NULL };
+static const char *brightness_up[]  = { "xbacklight", "-inc", "5", NULL };
+static const char *filepicker[]  = { "openfilepicker", NULL };
+static const char *screenshot[] = { "snapshot", "-s", NULL };
+static const char *videocapture[] = {"snapshot", "-v", NULL};
+static const char *omniclip_general[] = {"omniclip", "-g", NULL};
+static const char *omniclip_browse[] = {"omniclip", "-b", NULL};
+static const char *vpn_toggle[] = {"vpn", "-t", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -74,9 +96,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_z,      zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
@@ -88,6 +110,21 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+  { MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = voltoggle } },
+	{ MODKEY,                       XK_F2,     spawn,          {.v = voldown } },
+	{ MODKEY,                       XK_F3,     spawn,          {.v = volup } },
+	{ MODKEY,                       XK_F11,    spawn,          {.v = brightness_down } },
+	{ MODKEY,                       XK_F12,    spawn,          {.v = brightness_up } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = filepicker } },
+	{ 0     ,                       XK_Print,  spawn,          {.v = screenshot } },
+	{ MODKEY,			                  XK_Print,  spawn,	         {.v = videocapture } },
+	{ MODKEY,                       XK_o,      spawn,          {.v = omniclip_general } },
+	{ MODKEY|ShiftMask,             XK_o,      spawn,          {.v = omniclip_browse } },
+	{ MODKEY,                       XK_v,      spawn,          {.v = vpn_toggle } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
